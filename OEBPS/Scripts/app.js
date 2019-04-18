357797f254013bf860948540214074996899173f
Dan M <catullus1973@gmail.com>
2019-04-18 13:45:41 -0700

    Update app.js

--- OEBPS/Scripts/app.js
+++ OEBPS/Scripts/app.js
@@ -1,3 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("app is loaded");
-});
+(function expandCollapse() {
+
+    function setOpenBtns() {
+
+        var btnsOpen = document.querySelectorAll("[aria-expanded][aria-controls]"),
+            isCollapsed;
+
+        btnsOpen.forEach(function (btn, index) {
+            btn.addEventListener("click", function (e) {
+
+                isCollapsed = btn.getAttribute("aria-expanded") === "false";
+
+                if (isCollapsed) {
+                    btn.setAttribute("aria-expanded", "true");
+                } else {
+                    btn.setAttribute("aria-expanded", "false");
+                }
+
+            });
+        });
+    }
+
+    function setCloseBtns() {
+
+        var btnsClose = document.querySelectorAll("[name^='ecbox']"),
+            openBtn;
+
+        btnsClose.forEach(function (btn, index) {
+            btn.addEventListener("click", function (e) {
+
+                openBtn = document.querySelector("[aria-controls='" + btn.getAttribute('name') + "']");
+                openBtn.setAttribute("aria-expanded", "false");
+
+            });
+        });
+    }
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+
+        setOpenBtns();
+        setCloseBtns();
+
+    });
+
+
+})();
