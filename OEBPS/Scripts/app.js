27f359a42adc08c26e42850c28c5f4e629856a9e
Dan Moneypenny <catullus1973@gmail.com>
2019-04-25 01:55:17 -0700

    update

--- OEBPS/Scripts/app.js
+++ OEBPS/Scripts/app.js
@@ -2,37 +2,47 @@
 
     var toggleBtns = document.querySelectorAll("[aria-expanded][aria-controls]");
     var collapseBtns = document.querySelectorAll("[name^='ecbox']");
+    var ecContentItems = document.querySelectorAll("[id^=ecbox]");
 
-    function ecToggle(eventTarget) {
+    function ecToggle(eventTarget, ecContent) {
         var isCollapsed = eventTarget.getAttribute("aria-expanded") === "false";
-        var btnString = eventTarget.getAttribute("aria-controls");
-        var ecContent = document.getElementById(btnString);
-        console.log(btnString);
+        console.log("Toggle: ", ecContent); 
+        console.log("target: ", eventTarget);
+        
         if(isCollapsed) {
             eventTarget.setAttribute("aria-expanded", "true");
-            ecContent.style.display = "block";
+            ecContent.classList.replace("ec_collapsed", "ec_expanded");
         } else {
             eventTarget.setAttribute("aria-expanded", "false");
-            ecContent.style.display = "none";
-        }        
+            ecContent.classList.replace("ec_expanded", "ec_collapsed");
+        }  
+             
     }
 
-    function ecCollapse(eventTarget) {
-        var btnString = eventTarget.getAttribute("name");
-        var ecContent = document.getElementById(btnString);
-        var toggleBtn = document.querySelector("[aria-controls='" + btnString + "']");
-        ecContent.style.display = "none";
-        toggleBtn.setAttribute("aria-expanded", "false");
+    function ecCollapse(eventTarget, collapseContent, toggleBtn) {    
+        console.log("Collapse: ", collapseContent);
+        collapseContent.classList.replace("ec_expanded", "ec_collapsed");       
+        toggleBtn.setAttribute("aria-expanded", "false");        
     }
+
+    ecContentItems.forEach(function(item, index) {
+        console.log("item: ", index);
+        item.classList.add("ec_collapsed");
+    });
 
     toggleBtns.forEach(function(btn, index) {
         console.log("toggleBtn: ", index);
-        btn.addEventListener("click", function(e) {ecToggle(e.target)}, false);
+        var toggleString = btn.getAttribute("aria-controls");
+        var toggleContent = document.getElementById(toggleString);
+        btn.addEventListener("click", function(e) {ecToggle(e.target, toggleContent)}, false);
     });
     
     collapseBtns.forEach(function(btn, index) {
         console.log("collapseBtn: ", index);
-        btn.addEventListener("click", function(e) {ecCollapse(e.target)}, false);
+        var collapseString = btn.getAttribute("name");
+        var collapseContent = document.getElementById(collapseString); 
+        var toggleBtn = document.querySelector("[aria-controls='" + collapseString + "']")       
+        btn.addEventListener("click", function(e) {ecCollapse(e.target, collapseContent, toggleBtn)}, false);
     });     
  
 })();
