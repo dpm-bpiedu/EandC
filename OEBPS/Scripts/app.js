9f2a26ca417a251ef285e57a9cef4597c65d2dc2
Dan Moneypenny <catullus1973@gmail.com>
2019-04-30 00:06:23 -0700

    strip out markup

--- OEBPS/Scripts/app.js
+++ OEBPS/Scripts/app.js
@@ -1,39 +1,22 @@
 (function expandCollapse() {
 
-    var toggleBtns = document.querySelectorAll("[aria-expanded][aria-controls]"),ecString,ecContent,collapseBtn;
+  var ecBoxes = document.querySelectorAll("[data-ec='container']"),
+      ecString,
+      ecContent,
+      btn;
 
-    function ecToggle(current) {
-        var isCollapsed = current.getAttribute("aria-expanded") === "false";
-        var currentString = current.getAttribute("aria-controls");
-        var currentContent = document.getElementById(currentString);
-        if(isCollapsed) {
-            current.setAttribute("aria-expanded", "true");
-            currentContent.classList.replace("ec_collapsed", "ec_expanded");
-        } else {
-            current.setAttribute("aria-expanded", "false");
-            currentContent.classList.replace("ec_expanded", "ec_collapsed");
-        }
-    }
+  function makeBtn(idString) {    
+    btn = document.createElement("button");
+    console.log(btn);
+    return btn;
+  }
 
-    function ecCollapse(current) {
-        var currentString = current.getAttribute("name");
-        var currentToggle = document.querySelector("[aria-controls='" +  currentString + "']");
-        var currentContent = document.getElementById(currentString);
+  ecBoxes.forEach(function(ecBox, index) {
+    ecString = "ecbox_" + (index + 1);
+    ecContent = ecBox.querySelector("div");
+    ecContent.id = ecString;
+    makeBtn(ecString);
 
-        currentToggle.setAttribute("aria-expanded", "false");
-        currentContent.classList.replace("ec_expanded", "ec_collapsed");
+  });
 
-    }
-
-    toggleBtns.forEach(function(btn) {
-        ecString = btn.getAttribute("aria-controls");
-        ecContent = document.querySelector("#"+ ecString);
-        collapseBtn = document.querySelector("[name='" + ecString + "']");
-
-        ecContent.classList.add("ec_collapsed");
-        btn.addEventListener("click", function() {ecToggle(this)}, false);
-        collapseBtn.addEventListener("click", function() {ecCollapse(this)}, false);
-
-   });       
- 
 })();
