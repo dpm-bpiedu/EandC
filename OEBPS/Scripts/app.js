bda7e855d53ec9f4df7d6468fa1d2c3c3dc56814
Dan M <catullus1973@gmail.com>
2019-04-30 17:26:49 -0700

    Update app.js

--- OEBPS/Scripts/app.js
+++ OEBPS/Scripts/app.js
@@ -1,12 +1,22 @@
 (function expandCollapse() {
-
+  
+  function setAttrs(el, attrs) {
+    Object.keys(attrs).forEach(function (attr) {
+      el.setAttribute(attr, attrs[attr];
+    });
+  }
+                               
   var ecBoxes = document.querySelectorAll("[data-ec='container']"),
       ecString,
       ecContent,
       btn;
 
-  function makeBtn(idString) {    
+  function makeBtn(textString, attrs) {    
     btn = document.createElement("button");
+    btnText = document.createTextNode(textString);
+    btn.appendChild(btnText);
+    setAttrs(btn, attrs);
+    // add event listener
     console.log(btn);
     return btn;
   }
