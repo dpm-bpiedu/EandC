5f63f3c992884df4711b035c0136f04a8c8d5d9d
Dan Moneypenny <catullus1973@gmail.com>
2019-05-08 13:52:54 -0700

    refine app.js

--- DEV_SCRIPTS/app.js
+++ DEV_SCRIPTS/app.js
@@ -1,13 +1,11 @@
 (function expandCollapse() {
 
     var toggleBtns = document.querySelectorAll("[aria-expanded][aria-controls]");
-    var stringStem = "ecbox_";
     var isCollapsed, currentString, currentContent, currentToggle, ecString, ecContent, collapseBtn;
 
-    function ecToggle(currentObj, index) {
+    function ecToggle(currentObj) {
         isCollapsed = currentObj.getAttribute("aria-expanded") === "false";
-        currentString = stringStem + (++index);
-        currentContent = document.getElementById(currentString);
+        currentContent = document.getElementById(currentObj.getAttribute("aria-controls"));
         if(isCollapsed) {
             currentObj.setAttribute("aria-expanded", "true");
             currentContent.classList.replace("ec_collapsed", "ec_expanded");
@@ -17,8 +15,8 @@
         }
     }
 
-    function ecCollapse(current, index) {
-        currentString = stringStem + (++index);
+    function ecCollapse(currentObj) {
+        currentString = currentObj.getAttribute("name");
         currentToggle = document.querySelector("[aria-controls='" +  currentString + "']");
         currentContent = document.getElementById(currentString);
 
@@ -27,14 +25,14 @@
 
     }
 
-    toggleBtns.forEach(function(btn, index) {
+    toggleBtns.forEach(function(btn) {
         ecString = btn.getAttribute("aria-controls");
         ecContent = document.querySelector("#"+ ecString);
         collapseBtn = document.querySelector("[name='" + ecString + "']");
 
         ecContent.classList.add("ec_collapsed");
-        btn.addEventListener("click", function() {ecToggle(this, index)}, false);
-        collapseBtn.addEventListener("click", function() {ecCollapse(this, index)}, false);
+        btn.addEventListener("click", function() {ecToggle(this)}, false);
+        collapseBtn.addEventListener("click", function() {ecCollapse(this)}, false);
 
    });       
  
